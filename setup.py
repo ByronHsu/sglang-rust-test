1cf3669171c2ebaac0daa695c04d0673bc88ee4c
ByronHsu <byronhsu1230@gmail.com>
2024-11-10 08:10:21 +0000

    wip

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="sglang-rust-test",
-    version="0.1.7",
+    version="0.1.7.1",
     packages=find_packages(where="py_src"),
     package_dir={"": "py_src"},
     rust_extensions=[
